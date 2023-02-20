6415f57efd1dc8aef8e5a1d16a304a3ba0f262e5
mcerjak <mcerjak@85.cerfrance.fr>
2023-02-20 14:47:21 +0100

    userAuth

--- instantTchat/src/App.vue
+++ instantTchat/src/App.vue
@@ -1,86 +1,86 @@
 <template>
-  <div id="app">
-      <header>
-        <p class="logo-block">
-          <img src="./public/assets/logo-coq.png" alt="logo" />
-        </p>
-        <h1>InstanChat</h1>
-        <button @click="logout" class="logout-btn">Logout</button>
-      </header>
-      <main>
-          <sidebar :channels="channels" :members="members" />
-          <router-view :user="user" :channels="channels" />
-          <home />
-      </main>
-      </div>
+<div id="app">
+    <header>
+    <p class="logo-block">
+        <img src="./public/assets/logo-coq.png" alt="logo" />
+    </p>
+    <h1>InstanChat</h1>
+    <button @click="logout" class="logout-btn">Logout</button>
+    </header>
+    <main>
+        <sidebar :channels="channels" :members="members" />
+        <router-view :user="user" :channels="channels" />
+        <home />
+    </main>
+    </div>
 </template>
-  
+
 <script>
-  import Sidebar from './components/Sidebar.vue';
-  import Home from './components/Home.vue';
-  
-  export default {
-      name: 'App',
-      components: {
-      Sidebar,
-      Home
-      },
-      props: {
-      user: {
-          type: Object,
-          required: true
-      },
-      channels: {
-          type: Array,
-          required: true
-      },
-      members: {
-          type: Array,
-          required: true
-      }
-      },
-      methods: {
-      logout() {
-          // code pour déconnecter l'utilisateur
-      }
-      }
-  };
+import Sidebar from './components/Sidebar.vue';
+import Home from './components/Home.vue';
+
+export default {
+    name: 'App',
+    components: {
+    Sidebar,
+    Home
+    },
+    props: {
+    user: {
+        type: Object,
+        required: true
+    },
+    channels: {
+        type: Array,
+        required: true
+    },
+    members: {
+        type: Array,
+        required: true
+    }
+    },
+    methods: {
+    logout() {
+        // code pour déconnecter l'utilisateur
+    }
+    }
+};
 </script>
-  
+
 <style>
-  #app {
-      margin: 0;
-      display: flex;
-      flex-direction: column;
-      height: 100vh;
-      background-color: #333;
-  }
-  
-  main {
-      display: flex;
-      flex: 1;
-  }
+#app {
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    background-color: #333;
+}
 
-  .logout-btn {
-      background-color: #333;
-      color: #fff;
-      border: none;
-      padding: 5px 10px;
-      border-radius: 5px;
-      cursor: pointer;
-      font-size: 1.2rem;
-      height: 40px;
-      margin: auto 0;
-  }
+main {
+    display: flex;
+    flex: 1;
+}
 
-  .logo-block {
-      width: 60px;
-      height: 60px;
-      margin: auto 0;
-  }
+.logout-btn {
+    background-color: #333;
+    color: #fff;
+    border: none;
+    padding: 5px 10px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 1.2rem;
+    height: 40px;
+    margin: auto 0;
+}
 
-  .logo-block img {
-      width: 100%;
-      height: 100%;
-  }
+.logo-block {
+    width: 60px;
+    height: 60px;
+    margin: auto 0;
+}
+
+.logo-block img {
+    width: 100%;
+    height: 100%;
+}
 </style>
